56cadd6a54ae9b67cb74e9c62a11a53fbee24dd1
kakahan01 <34622630+kakahan01@users.noreply.github.com>
2018-07-03 23:47:06 +0300

    o

---
+++ commands/emptyEvent.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+
+module.exports.run = async (array, client) => {
+    /*
+    array = params
+    */
+}
--- run.js
+++ run.js
@@ -3,6 +3,52 @@ const Discord = require('discord.js');
 const dc = require('./index.js');
 const client = new Discord.Client();
 
+const events = [ 
+"channelCreate",
+"channelDelete",
+"channelPinsUpdate",
+"channelUpdate",
+"clientUserGuildSettingsUpdate",
+"clientUserSettingsUpdate",
+"debug",
+"disconnect",
+"emojiCreate",
+"emojiDelete",
+"emojiUpdate",
+"error",
+"guildBanAdd",
+"guildBanRemove",
+"guildCreate",
+"guildDelete",
+"guildMemberAdd",
+"guildMemberAvailable",
+"guildMemberRemove",
+"guildMembersChunk",
+"guildMemberSpeaking",
+"guildMemberUpdate",
+"guildUnavailable",
+"guildUpdate",
+"messageDelete",
+"messageDeleteBulk",
+"messageReactionAdd",
+"messageReactionRemove",
+"messageReactionRemoveAll",
+"messageUpdate",
+"presenceUpdate",
+"ready",
+"reconnecting",
+"resume",
+"roleCreate",
+"roleDelete",
+"roleUpdate",
+"typingStart",
+"typingStop",
+"userNoteUpdate",
+"userUpdate",
+"voiceStateUpdate",
+"warn"
+];
+
 client.login(config["token"]);
 
 client.on('message', async (message) => {
@@ -16,7 +62,23 @@ client.on('message', async (message) => {
     var args = message.content.slice(prefix.length).split(" ");
     const command = args[0];
     
+    if (events.some(e => command == e)) return;
     dcPlatform.run(command, message);
+});
+
+client.on('channelCreate', async (channel) => {
+    const dcPlatform = new dc.DCPlatform(client);
+    dcPlatform.event([channel], "channelCreate");
+});
+
+client.on('channelDelete', async (channel) => {
+    const dcPlatform = new dc.DCPlatform(client);
+    dcPlatform.event([channel], "channelDelete");
+});
+
+client.on('channelPinsUpdate', async (channel, time) => {
+    const dcPlatform = new dc.DCPlatform(client);
+    dcPlatform.event([channel, time], "channelPinsUpdate");
 });
 
 /*
@@ -33,7 +95,11 @@ client.on('message', async (message) => {
         "exdisable"
     ]),;
 
-    const dcPlatform = new dc.DCPlatform(client);
+    const commandGroups = [
+        group
+    ];
+
+    const dcPlatform = new dc.DCPlatform(client, commandGroups);
 
     var args = message.content.slice(prefix.length).split(" ");
     const command = args[0];
